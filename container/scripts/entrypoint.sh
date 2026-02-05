fc81949e6fa838cfde237f8f234d2df207e0478a
Prabhjeet Singh <dev@prabhjeet.me>
2026-02-05 20:33:37 +0000

    VPN: WireGuard configured

---
+++ container/scripts/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+
+# Load utility
+source /etc/openjanus/scripts/util.sh
+
+log "info" "EntryPoint" "Configuring WireGuard..."
+
+# Configure WireGuard
+bash /etc/openjanus/scripts/wireguard.sh
+
+log "info" "EntryPoint" "Configuring SSL..."
+
+# Configure SSL
+bash /etc/openjanus/scripts/ssl.sh
+
+log "success" "EntryPoint" "Success"
+
+# Execute the command passed to CMD or other application logic
+exec "$@"
