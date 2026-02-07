30a528b66bd42894678d8accc1c81a712f90f4f8
Prabhjeet Singh <dev@prabhjeet.me>
2026-02-07 09:17:30 +0000

    Overall: Textual changes

--- container/scripts/renew_reload.sh
+++ container/scripts/renew_reload.sh
@@ -4,18 +4,18 @@ set -euo pipefail
 # Load utility
 source /etc/openjanus/scripts/util.sh
 
-log "info" "SSL" "Renewing certificates..."
+log "info" "SSL RENEW" "Renewing certificates..."
 
 if [[ "$CB_TESTING" == "1"]]; then
-  log "warning" "SSL" "Test Mode: Using certbot's dry-run command & simulating certificate renewal."
+  log "warning" "SSL RENEW" "Test Mode: Using certbot's dry-run command & simulating certificate renewal."
 fi
 
 # Renew certificates
 certbot renew --webroot -w /var/www/certbot $( [ "$CB_TESTING" = "1" ] && echo --dry-run )
 
-log "info" "SSL" "Reloading Nginx..."
+log "info" "SSL RENEW" "Reloading Nginx..."
 
 # Restart nginx
-nginx -s reload || log "error" "SSL" "Nginx reload failed!"
+nginx -s reload || log "error" "SSL RENEW" "Nginx reload failed!"
 
-log "success" "SSL" "Due certificate renewed."
+log "success" "SSL RENEW" "Due certificate renewed."
